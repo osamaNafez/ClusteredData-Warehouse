506c0ef6dbfc5fb4e0cc2f3d7bcf77c5b392d4dc
Osama <osamashatat99@gmail.com>
2023-06-21 23:59:50 +0300

    initialize project

---
+++ src/test/java/com/progressoft/clustereddata/warehouse/clustereddatawarehouse/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressoft.clustereddata.warehouse.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
